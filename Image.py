566efb8c266a90fd4832708dabb0da33373b41ce
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-09-16 13:39:53 -0500

    GG

---
+++ Image.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from PIL import Image , ImageDraw , ImageTk
+blanco = (255, 255, 0)
+
+image1 = Image.new("RGB", (400,300) , blanco)
+pintura = ImageDraw.Draw(image1)
+
+print(type(pintura))
+
+pintura.line( [10,0,10,100] , fill="black" )
+pintura.line( [10,100,100,100] , fill="black" )
+pintura.rectangle( [110,110,150,150] , fill="black" )
+
+filename = "Prueba.png"
+image1.save(filename)
+
+pantaya = Tk()
+imagen = Image.open("Prueba.png")
+nuevoancho = 150
+
+alto,ancho = 500,500
+
+foto = ImageTk.PhotoImage(imagen)
+canvas = Canvas(pantaya, height=alto , width=ancho)
+canvas.create_image(100, 80, image=foto)
+
+canvas.pack(side = TOP, expand= True , fill=BOTH)
+pantaya.mainloop()
